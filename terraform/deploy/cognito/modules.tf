ed0232a0850dc2a0d09c9653f8aebe101cf5b4b0
Fernando Schmidt <fernando.schmidt@dmwgroup.co.uk>
2020-05-15 14:48:22 +0100

    DW-4008 Added Pre Auth Lambda to Cognito User Pool

--- terraform/deploy/cognito/modules.tf
+++ terraform/deploy/cognito/modules.tf
@@ -7,8 +7,9 @@ module "cognito" {
     "jupyterhub",
   ]
 
-  root_dns_names = values(local.root_dns_name)
-  domain         = local.cognito_domain
+  root_dns_names  = values(local.root_dns_name)
+  domain          = local.cognito_domain
+  pre_auth_lambda = data.terraform_remote_state.analytical-frontend-service.outputs.pre_auth_lambda.arn
 }
 
 module "cognito-fs" {
