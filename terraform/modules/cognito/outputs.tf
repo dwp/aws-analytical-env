976be7edad6681261876103f74b0d7fd47462faa
MattToczek <52243130+MattToczek@users.noreply.github.com>
2020-05-15 13:15:51 +0100

    adding user pool arn as output to be used in custom auth lambda triggers (#8)

--- terraform/modules/cognito/outputs.tf
+++ terraform/modules/cognito/outputs.tf
@@ -3,5 +3,6 @@ output outputs {
     app_clients      = aws_cognito_user_pool_client.app_client[*]
     user_pool_domain = aws_cognito_user_pool_domain.main.domain
     user_pool_id     = aws_cognito_user_pool.emr.id
+    user_pool_arn    = aws_cognito_user_pool.emr.arn
   }
 }
