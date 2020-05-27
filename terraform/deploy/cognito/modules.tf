0d4ba9d67e9cab4d6461e453d5524b58dfb2e74c
Fernando Schmidt <fernando.schmidt@dmwgroup.co.uk>
2020-05-27 15:06:20 +0100

    DW-4009 Added Lambda to check for user about to expire

--- terraform/deploy/cognito/modules.tf
+++ terraform/deploy/cognito/modules.tf
@@ -33,3 +33,14 @@ module "pre-auth-lambda" {
   common_tags   = local.common_tags
   user_pool_arn = module.cognito-fs.outputs.user_pool_arn
 }
+
+module "check-user-expiry-lambda" {
+  source = "../../modules/check-user-expiry-lambda"
+
+  name_prefix              = var.name_prefix
+  common_tags              = local.common_tags
+  cognito_user_pool_id     = data.terraform_remote_state.aws_analytical_env_cognito.outputs.cognito-fs.user_pool_id
+  dynamodb_table_user_arn  = module.pre-auth-lambda.dynamodb_table_user.arn
+  dynamodb_table_user_name = module.pre-auth-lambda.dynamodb_table_user.name
+  from_email_address       = "DataWorks Access Management <access-management@${data.terraform_remote_state.aws_common_infrastructure.outputs.domain_identity}>"
+}
