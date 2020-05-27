4a264cdbad9e983e8deaa6867c1340e55d3a6893
draagc-iw <55280269+draagc-iw@users.noreply.github.com>
2020-05-27 11:07:00 +0100

    Fix Auth lambdas IAM permissions (#23)
    * Fix handlers and IAM policies
    
    * Increase timeout to 6 seconds

--- terraform/modules/custom-auth-flow/custom_auth_flow_lambdas.tf
+++ terraform/modules/custom-auth-flow/custom_auth_flow_lambdas.tf
@@ -2,8 +2,9 @@ resource "aws_lambda_function" "lambda_create_challenge" {
   filename         = var.custom_auth_file_path
   function_name    = "${var.name_prefix}-create-challenge"
   role             = aws_iam_role.role_for_lambda_create_auth_challenge.arn
-  handler          = "module.exports.createAuthChallenge"
+  handler          = "lambda.createAuthChallenge"
   runtime          = "nodejs12.x"
+  timeout          = 6
   source_code_hash = filebase64sha256(var.custom_auth_file_path)
   tags             = merge(var.common_tags, { Name = "${var.name_prefix}-create-challenge" })
 }
@@ -12,8 +13,9 @@ resource "aws_lambda_function" "lambda_define_challenge" {
   filename         = var.custom_auth_file_path
   function_name    = "${var.name_prefix}-define-challenge"
   role             = aws_iam_role.role_for_lambda_define_auth_challenge.arn
-  handler          = "module.exports.defineAuthChallenge"
+  handler          = "lambda.defineAuthChallenge"
   runtime          = "nodejs12.x"
+  timeout          = 6
   source_code_hash = filebase64sha256(var.custom_auth_file_path)
   tags             = merge(var.common_tags, { Name = "${var.name_prefix}-define-challenge" })
 }
@@ -22,8 +24,9 @@ resource "aws_lambda_function" "lambda_verify_challenge" {
   filename         = var.custom_auth_file_path
   function_name    = "${var.name_prefix}-verify-challenge"
   role             = aws_iam_role.role_for_lambda_verify_auth_challenge.arn
-  handler          = "module.exports.verifyAuthChallenge"
+  handler          = "lambda.verifyAuthChallenge"
   runtime          = "nodejs12.x"
+  timeout          = 6
   source_code_hash = filebase64sha256(var.custom_auth_file_path)
   tags             = merge(var.common_tags, { Name = "${var.name_prefix}-verify-challenge" })
 }
--- terraform/modules/custom-auth-flow/iam.tf
+++ terraform/modules/custom-auth-flow/iam.tf
@@ -23,16 +23,12 @@ data aws_iam_policy_document assume_role_lambda_custom_auth_flow_document {
   }
 }
 
-resource "aws_iam_role_policy_attachment" "create_challenge_sns_role_policy_attach" {
-  role       = aws_iam_role.role_for_lambda_create_auth_challenge.name
-  policy_arn = aws_iam_policy.cognito_auth_sns_policy.arn
+resource aws_iam_role_policy cognito_create_auth_policy {
+  policy = data.aws_iam_policy_document.cognito_create_auth_policy_document.json
+  role   = aws_iam_role.role_for_lambda_create_auth_challenge.name
 }
 
-resource aws_iam_policy cognito_auth_sns_policy {
-  policy = data.aws_iam_policy_document.cognito_auth_sns_policy_document.json
-}
-
-data aws_iam_policy_document cognito_auth_sns_policy_document {
+data aws_iam_policy_document cognito_create_auth_policy_document {
   statement {
     effect = "Allow"
     actions = [
@@ -49,58 +45,32 @@ data aws_iam_policy_document cognito_auth_sns_policy_document {
   }
 }
 
-resource "aws_iam_role_policy_attachment" "cognito_create_challenge_trigger_policy_attach" {
+resource "aws_iam_role_policy_attachment" "cognito_create_challenge_basic_execution" {
   role       = aws_iam_role.role_for_lambda_create_auth_challenge.name
-  policy_arn = aws_iam_policy.cognito_challenge_trigger_policy.arn
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-resource "aws_iam_role_policy_attachment" "cognito_verify_challenge_trigger_policy_attach" {
+resource "aws_iam_role_policy_attachment" "cognito_verify_challenge_basic_execution" {
   role       = aws_iam_role.role_for_lambda_verify_auth_challenge.name
-  policy_arn = aws_iam_policy.cognito_challenge_trigger_policy.arn
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-resource "aws_iam_role_policy_attachment" "cognito_define_challenge_trigger_policy_attach" {
+resource "aws_iam_role_policy_attachment" "cognito_define_challenge_trigger_basic_execution" {
   role       = aws_iam_role.role_for_lambda_define_auth_challenge.name
-  policy_arn = aws_iam_policy.cognito_challenge_trigger_policy.arn
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-resource aws_iam_policy cognito_challenge_trigger_policy {
-  policy = data.aws_iam_policy_document.cognito_challenge_trigger_document.json
-}
 
-data aws_iam_policy_document cognito_challenge_trigger_document {
-  statement {
-    effect = "Allow"
-    actions = [
-      "logs:CreateLogGroup"
-    ]
-    resources = [
-      "arn:aws:logs:${var.region}:${var.account}:*"
-    ]
-  }
-  statement {
-    effect = "Allow"
-    actions = [
-      "logs:CreateLogStream",
-      "logs:PutLogEvents"
-    ]
-    resources = [
-      "arn:aws:logs:${var.region}:${var.account}:log-group:/aws/lambda/*:*"
-    ]
-  }
-}
-
-resource aws_iam_role_policy cognito_define_auth_trigger_policy {
-  policy = data.aws_iam_policy_document.cognito_define_auth_trigger_document.json
+resource aws_iam_role_policy cognito_define_auth_policy {
+  policy = data.aws_iam_policy_document.cognito_define_auth_policy.json
   role   = aws_iam_role.role_for_lambda_define_auth_challenge.name
 }
 
-data aws_iam_policy_document cognito_define_auth_trigger_document {
+data aws_iam_policy_document cognito_define_auth_policy {
   statement {
     effect = "Allow"
     actions = [
-      "cognito-idp:Get*",
-      "cognito-identity:Get*"
+      "cognito-idp:AdminGetUser",
     ]
     resources = [
       var.cognito_user_pool_arn
