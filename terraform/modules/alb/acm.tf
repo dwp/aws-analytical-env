f87099b2095fd05589cb04067f06a24af9e66871
caseyr232 <2818278+caseyr232@users.noreply.github.com>
2020-05-29 15:45:07 +0100

    Revert "DW-4172_Deploy (#37)"
    This reverts commit 8650556b1bbb0ffb1b1885c9bfc7f181454fbf8d.

--- terraform/modules/alb/acm.tf
+++ terraform/modules/alb/acm.tf
@@ -12,20 +12,3 @@ resource "aws_acm_certificate" "analytical-alb-cert" {
 
   tags = merge(var.common_tags, { Name = "${var.name_prefix}-lb" })
 }
-
-//can be deleted after apply
-
-resource "aws_acm_certificate" "cert" {
-  domain_name               = local.fqdn
-  certificate_authority_arn = var.cert_authority_arn
-
-  options {
-    certificate_transparency_logging_preference = "DISABLED"
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  tags = merge(var.common_tags, { Name = "${var.name_prefix}-lb" })
-}
