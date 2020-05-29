f3f9d89b94096a182ee452feb9222f6ad06c7c97
Ian Utley <ian.utley@infinityworks.com>
2020-05-29 16:50:24 +0100

    Fix env var assignment (#41)

--- terraform/modules/emr/templates/emr/setup.sh
+++ terraform/modules/emr/templates/emr/setup.sh
@@ -44,12 +44,12 @@ for GROUP in $${COGNITO_GROUPS[@]}; do
   echo "Adding users for group $GROUP"
   USERS=$(aws cognito-idp list-users-in-group --user-pool-id "${user_pool_id}" --group-name "$GROUP" | jq '.Users' | jq -r '.[].Username')
 
-  USERDIR = $(aws cognito-idp list-users --user-pool-id "${user_pool_id}")
+  USERDIR=$(aws cognito-idp list-users --user-pool-id "${user_pool_id}")
 
   for USER in $${USERS[@]}; do
 
     # Convert username format
-    USER = $(echo $USERDIR \
+    USER=$(echo $USERDIR \
             | jq ".Users[] as \$u | if \$u.Username == \"$USER\" then \$u else empty end" \
             | jq -r ".Attributes[] | if .Name == \"sub\" then \"$USER\" + (.Value | match(\"...\").string) else empty end")
 
