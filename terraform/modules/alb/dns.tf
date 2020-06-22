0c0d94a285eb7058e840592a9efbd0e47a577564
Casey Rogers <2818278+caseyr232@users.noreply.github.com>
2020-06-22 12:51:43 +0100

    DW-4370: Correct validation options

--- terraform/modules/alb/dns.tf
+++ terraform/modules/alb/dns.tf
@@ -15,9 +15,9 @@ resource "aws_route53_record" "record" {
 resource "aws_route53_record" "record_acm_verify" {
   provider = aws.management-dns
 
-  name    = aws_acm_certificate.analytical-alb-cert-pub.resource_record_name
-  type    = aws_acm_certificate.analytical-alb-cert-pub.resource_record_type
+  name    = aws_acm_certificate.analytical-alb-cert-pub.domain_validation_options.resource_record_name
+  type    = aws_acm_certificate.analytical-alb-cert-pub.domain_validation_options.resource_record_type
   zone_id = data.aws_route53_zone.main.zone_id
 
-  records = [aws_acm_certificate.analytical-alb-cert-pub.resource_record_value]
+  records = [aws_acm_certificate.analytical-alb-cert-pub.domain_validation_options.resource_record_value]
 }
