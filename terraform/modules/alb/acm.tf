8fa99f6b736b2d508fa88108b3e38638995792dd
Casey Rogers <2818278+caseyr232@users.noreply.github.com>
2020-06-22 15:23:18 +0100

    DW-4370: Remove (now) unused cert
    Signed-off-by: Casey Rogers <2818278+caseyr232@users.noreply.github.com>

--- terraform/modules/alb/acm.tf
+++ terraform/modules/alb/acm.tf
@@ -1,18 +1,3 @@
-resource "aws_acm_certificate" "analytical-alb-cert" {
-  domain_name               = local.fqdn
-  certificate_authority_arn = var.cert_authority_arn
-
-  options {
-    certificate_transparency_logging_preference = "DISABLED"
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  tags = merge(var.common_tags, { Name = "${var.name_prefix}-lb" })
-}
-
 resource "aws_acm_certificate" "analytical-alb-cert-pub" {
   domain_name       = local.fqdn
   validation_method = "DNS"
