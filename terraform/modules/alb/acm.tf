7611cf00602be4fa0657ba4047299d9156aad7a9
caseyr232 <2818278+caseyr232@users.noreply.github.com>
2020-06-22 14:17:04 +0100

    DW-4370: AWS Certs (#69)
    * DW-4370: AWS Certs
    
    * DW-4370: Add DNS Verify for ACM
    
    * DW-4370: Correct validation options
    
    * DW-4370: Correct validation options
    
    * DW-4370: Add line ending

--- terraform/modules/alb/acm.tf
+++ terraform/modules/alb/acm.tf
@@ -12,3 +12,18 @@ resource "aws_acm_certificate" "analytical-alb-cert" {
 
   tags = merge(var.common_tags, { Name = "${var.name_prefix}-lb" })
 }
+
+resource "aws_acm_certificate" "analytical-alb-cert-pub" {
+  domain_name       = local.fqdn
+  validation_method = "DNS"
+
+  options {
+    certificate_transparency_logging_preference = "ENABLED"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = merge(var.common_tags, { Name = "${var.name_prefix}-lb" })
+}
