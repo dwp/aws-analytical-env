7299c171dc9834279d5771e055b5e3922afbf2ff
Levan K <37900502+TheDogfather17@users.noreply.github.com>
2020-07-01 10:21:47 +0100

    attaching kms policy document for decrypting published bucket (#80)
    * attaching kms policy document for decrypting published bucket
    
    * fix formatting
    
    * removing duplicate policies
    
    * Update terraform/modules/emr/emrfs_iam.tf
    
    Co-authored-by: Matthew Burgess <549318+mattburgess@users.noreply.github.com>
    
    Co-authored-by: Matthew Burgess <549318+mattburgess@users.noreply.github.com>

--- terraform/modules/emr/emrfs_iam.tf
+++ terraform/modules/emr/emrfs_iam.tf
@@ -18,6 +18,12 @@ data "aws_iam_policy_document" "emrfs_iam_assume_role" {
   }
 }
 
+resource "aws_iam_role_policy_attachment" "AnalyticalDatasetReadOnly" {
+  role       = aws_iam_role.emrfs_iam.name
+  policy_arn = "arn:aws:iam::${var.account}:policy/AnalyticalDatasetCrownReadOnly"
+}
+
+
 resource "aws_iam_role_policy" "emrfs_iam" {
   name   = "emrfs_iam"
   role   = aws_iam_role.emrfs_iam.id
@@ -34,58 +40,5 @@ data "aws_iam_policy_document" "emrfs_iam" {
     resources = [
       "*"
     ]
-  }
-
-  statement {
-    sid    = "AllowS3BucketList"
-    effect = "Allow"
-
-    actions = [
-      "s3:ListBucket"
-    ]
-
-    resources = concat(
-      [
-        for path_tuple in var.dataset_s3_paths :
-        format("arn:aws:s3:::%s", path_tuple[0])
-      ]
-    )
-  }
-
-  statement {
-    sid    = "AllowS3Read"
-    effect = "Allow"
-
-    actions = [
-      "s3:Get*",
-      "s3:List*"
-    ]
-
-    resources = concat(
-      [
-        for path_tuple in var.dataset_s3_paths :
-        format("arn:aws:s3:::%s/%s", path_tuple[0], path_tuple[1])
-      ]
-    )
-
-    condition {
-      test     = "StringEquals"
-      variable = format("s3:ExistingObjectTag/%s", var.dataset_s3_tags[0])
-
-      values = [
-        var.dataset_s3_tags[1]
-      ]
-    }
-  }
-  statement {
-    sid    = "AllowKMSRead"
-    effect = "Allow"
-
-    actions = [
-      "kms:Decrypt",
-      "kms:DescribeKey"
-    ]
-
-    resources = var.emrfs_kms_key_arns
   }
 }
