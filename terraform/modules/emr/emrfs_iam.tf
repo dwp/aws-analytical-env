fef8bfa973e2b2c13727a9737403356b0b2b0e67
MattToczek <matthew.toczek@dmwgroup.co.uk>
2020-07-16 11:02:33 +0100

    removing unused outputs and adding non_pii version of emrfs role

--- terraform/modules/emr/emrfs_iam.tf
+++ terraform/modules/emr/emrfs_iam.tf
@@ -4,6 +4,12 @@ resource "aws_iam_role" "emrfs_iam" {
   tags               = var.common_tags
 }
 
+resource "aws_iam_role" "emrfs_iam_non_pii" {
+  name               = "emrfs_iam_non_pii"
+  assume_role_policy = data.aws_iam_policy_document.emrfs_iam_assume_role.json
+  tags               = var.common_tags
+}
+
 data "aws_iam_policy_document" "emrfs_iam_assume_role" {
   statement {
     sid     = "AllowAssumeRole"
@@ -23,11 +29,21 @@ resource "aws_iam_role_policy_attachment" "AnalyticalDatasetReadOnly" {
   policy_arn = "arn:aws:iam::${var.account}:policy/AnalyticalDatasetCrownReadOnly"
 }
 
+resource "aws_iam_role_policy_attachment" "AnalyticalDatasetReadOnlyNonPii" {
+  role       = aws_iam_role.emrfs_iam_non_pii.name
+  policy_arn = "arn:aws:iam::${var.account}:policy/AnalyticalDatasetCrownReadOnly"
+}
 
 resource "aws_iam_role_policy" "emrfs_iam" {
   name   = "emrfs_iam"
   role   = aws_iam_role.emrfs_iam.id
   policy = data.aws_iam_policy_document.emrfs_iam.json
+}
+
+resource "aws_iam_role_policy" "emrfs_iam_non_pii" {
+  name   = "emrfs_iam_non_pii"
+  role   = aws_iam_role.emrfs_iam_non_pii.id
+  policy = data.aws_iam_policy_document.emrfs_iam_non_pii.json
 }
 
 data "aws_iam_policy_document" "emrfs_iam" {
@@ -40,5 +56,26 @@ data "aws_iam_policy_document" "emrfs_iam" {
     resources = [
       "*"
     ]
+  }
+}
+
+data "aws_iam_policy_document" "emrfs_iam_non_pii" {
+  statement {
+    sid    = "AllowAllDynamoDB"
+    effect = "Allow"
+    actions = [
+      "dynamodb:*",
+    ]
+    resources = [
+      "*"
+    ]
+    condition {
+      test     = "StringEquals"
+      variable = "s3:ExistingObjectTag/pii"
+
+      values = [
+        "false"
+      ]
+    }
   }
 }
