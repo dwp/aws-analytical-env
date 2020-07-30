1d1fa48d266bc00dcc7329635a925dcd8a6be5fe
Dragos Costache <dragos.costache@infinityworks.com>
2020-07-30 19:26:38 +0100

    Add ADFS provider name output

--- terraform/modules/cognito-fs/outputs.tf
+++ terraform/modules/cognito-fs/outputs.tf
@@ -1,7 +1,8 @@
 output outputs {
   value = {
-    user_pool_domain = aws_cognito_user_pool_domain.main.domain
-    user_pool_id     = aws_cognito_user_pool.emr.id
-    user_pool_arn    = aws_cognito_user_pool.emr.arn
+    user_pool_domain            = aws_cognito_user_pool_domain.main.domain
+    user_pool_id                = aws_cognito_user_pool.emr.id
+    user_pool_arn               = aws_cognito_user_pool.emr.arn
+    adfs_identity_provider_name = aws_cognito_identity_provider.adfs_dwp.provider_name
   }
 }
