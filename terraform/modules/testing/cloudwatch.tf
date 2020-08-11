eb43a7d212126e472fe3fdbac3cb6f115f4a08a1
Liam Rae-McLauchlan <liam.rae-mclauchlan@infinityworks.com>
2020-08-11 14:04:10 +0100

    Add CW events and targets

--- terraform/modules/testing/cloudwatch.tf
+++ terraform/modules/testing/cloudwatch.tf
@@ -22,3 +22,4 @@ resource "aws_lambda_permission" "allow_cloudwatch_to_call_metrics_lambda" {
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.every_five_minutes.arn
 }
+
