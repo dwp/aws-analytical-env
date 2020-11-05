0e1a827db848f9c67b4b7c7c8ed0979cdc9de0df
MattToczek <toczek.matt@gmail.com>
2020-11-05 16:10:51 +0000

    removing hardcoded username

--- terraform/deploy/app/modules.tf
+++ terraform/deploy/app/modules.tf
@@ -56,7 +56,7 @@ module "emr" {
   hive_metastore_endpoint      = data.terraform_remote_state.aws-analytical-dataset-generation.outputs.hive_metastore.rds_cluster.endpoint
   hive_metastore_database_name = data.terraform_remote_state.aws-analytical-dataset-generation.outputs.hive_metastore.rds_cluster.database_name
   hive_metastore_password      = jsondecode(data.aws_secretsmanager_secret_version.hive_metastore_password_secret.secret_string)["password"]
-  hive_metastore_username      = "analytical-env"
+  hive_metastore_username      = jsondecode(data.aws_secretsmanager_secret_version.hive_metastore_password_secret.secret_string)["username"]
   hive_metastore_sg_id         = data.terraform_remote_state.aws-analytical-dataset-generation.outputs.hive_metastore.security_group.id
 
   artefact_bucket = {
