cce63b8556156bc8e966922e59e1faebe31ce906
IanUtley <ianutley@digital.uc.dwp.gov.uk>
2020-11-09 15:59:00 +0000

    Add ARN

--- terraform/modules/emr/iam.tf
+++ terraform/modules/emr/iam.tf
@@ -106,7 +106,7 @@ data "aws_iam_policy_document" "elastic_map_reduce_role" {
     resources = [
       aws_iam_role.emr_ec2_role.arn,
       aws_iam_instance_profile.emr_ec2_role.arn, 
-      aws_iam_role.emr_autoscaling_role
+      aws_iam_role.emr_autoscaling_role.arn
     ]
   }
 
