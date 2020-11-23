6fb2843fc631db2368ff034b65d662b73b0c633c
MattToczek <toczek.matt@gmail.com>
2020-11-23 15:46:47 +0000

    new bucket for tables, master kms and, iam roles for groups

--- terraform/modules/emr/s3.tf
+++ terraform/modules/emr/s3.tf
@@ -73,3 +73,82 @@ resource "aws_s3_bucket_policy" "config_bucket_https_only" {
   bucket = aws_s3_bucket.emr.id
   policy = data.aws_iam_policy_document.config_bucket_https_only.json
 }
+
+
+
+resource "random_id" "hive_data_bucket_name" {
+  byte_length = 16
+}
+
+resource "aws_s3_bucket" "hive_data" {
+  bucket = random_id.hive_data_bucket_name.hex
+  acl    = "private"
+
+  tags = merge({ "Name" = "${var.emr_cluster_name}-hive-data-s3" }, var.common_tags)
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_alias.hive_data_s3.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+  lifecycle_rule {
+    id      = ""
+    prefix  = "/"
+    enabled = true
+
+    noncurrent_version_expiration {
+      days = 30
+    }
+  }
+
+  logging {
+    target_bucket = var.logging_bucket
+    target_prefix = "S3Logs/${var.name_prefix}-hive_data-s3-bucket"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "hive_data" {
+  bucket = aws_s3_bucket.hive_data.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
+}
+
+data "aws_iam_policy_document" "hive_data_bucket_https_only" {
+  statement {
+    sid     = "BlockHTTP"
+    effect  = "Deny"
+    actions = ["*"]
+
+    resources = [
+      aws_s3_bucket.hive_data.arn,
+      "${aws_s3_bucket.hive_data.arn}/*",
+    ]
+
+    principals {
+      identifiers = ["*"]
+      type        = "AWS"
+    }
+
+    condition {
+      test     = "Bool"
+      values   = ["false"]
+      variable = "aws:SecureTransport"
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "hive_data_bucket_https_only" {
+  bucket = aws_s3_bucket.hive_data.id
+  policy = data.aws_iam_policy_document.hive_data_bucket_https_only.json
+}
+
