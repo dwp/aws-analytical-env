f59614fdb67de89c2aadfa0205ada8613a2ec065
abdu-n <57192333+abdu-n@users.noreply.github.com>
2020-12-08 09:38:31 +0000

    Dw 5338 (#206)
    * DW-5338 add shell script for copying files
    
    * DW-5338
    
    * DW-5338
    
    * DW-5338 new line
    
    * DW-5338 address pr
    
    * DW-5338 ran terraform fmt
    
    * DW-5338 changed logging.sh
    
    * DW-5338 fixed terraform error
    
    Co-authored-by: abdurrahmannuhu <abdurrahman.nuhu@dwp.gsi.gov.ukgit config --global user.name abdurrahmannuhugit config --global user.email abdurrahman.nuhu@dwp.gsi.gov.uk>

---
+++ terraform/modules/emr/templates/emr/cloudwatch.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -Eeuo pipefail
+
+cwa_metrics_collection_interval="$1"
+cwa_log_group_name="$2"
+aws_default_region="$3"
+cwa_namespace="$4"
+
+# Create config file required for CloudWatch Agent
+mkdir -p /opt/aws/amazon-cloudwatch-agent/etc
+cat > /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json <<CWAGENTCONFIG
+{
+  "agent": {
+    "metrics_collection_interval": "$${cwa_metrics_collection_interval}",
+    "logfile": "/opt/aws/amazon-cloudwatch-agent/logs/amazon-cloudwatch-agent.log"
+  },
+  "logs": {
+    "logs_collected": {
+      "files": {
+        "collect_list": [
+          {
+            "file_path": "/var/log/get_scripts.log",
+            "log_group_name": "$${cwa_log_group_name}",
+            "log_stream_name": "get_scripts.log",
+            "timezone": "UTC"
+          }
+        ]
+      }
+    },
+    "log_stream_name": "$${cwa_namespace}",
+    "force_flush_interval": 15
+  }
+}
+CWAGENTCONFIG
+
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
+sudo systemctl start amazon-cloudwatch-agent
+
+
+
---
+++ terraform/modules/emr/templates/emr/get_scripts.sh
@@ -0,0 +1,28 @@
+SOURCE_LOCATION=$1
+DESTINATION_LOCATION=$2
+
+BUCKET="${config_bucket}"
+SOURCE_PATH=$BUCKET/$SOURCE_LOCATION
+
+(
+    # Import the logging functions
+    source /opt/emr/logging.sh
+
+    START_MESSAGE="Start Downloading files from $SOURCE_LOCATION to $DESTINATION_LOCATION"
+    log_message $START_MESSAGE "INFO" "NOT_SET" "$${PID}" "batch_emr" "get_scripts.sh" "NOT_SET"
+
+    aws s3 cp $SOURCE_PATH $DESTINATION_LOCATION --recursive
+    
+    END_MESSAGE="Finish Downloading files from $SOURCE_LOCATION to $DESTINATION_LOCATION"
+    log_message $END_MESSAGE "INFO" "NOT_SET" "$${PID}" "batch_emr" "get_scripts.sh" "NOT_SET"
+
+)  >> /var/log/get_scripts.log 2>&1
+
+
+
+
+
+
+
+
+
