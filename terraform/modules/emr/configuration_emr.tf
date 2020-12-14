cff751fd2c1c3809e32f7d01fdc1a48a357d525c
Ian Utley <ianutley@digital.uc.dwp.gov.uk>
2020-12-14 10:00:30 +0000

    Copy step.sh to EMR (#228)

--- terraform/modules/emr/configuration_emr.tf
+++ terraform/modules/emr/configuration_emr.tf
@@ -39,6 +39,7 @@ data "template_file" "emr_setup_sh" {
     cloudwatch_shell                = format("s3://%s/%s", aws_s3_bucket.emr.id, aws_s3_bucket_object.cloudwatch_sh.key)
     cwa_namespace                   = local.cw_agent_namespace
     cwa_log_group_name              = local.cw_agent_log_group_name
+    config_bucket                   = var.config_bucket_id
     aws_default_region              = "eu-west-2"
     cwa_metrics_collection_interval = local.cw_agent_metrics_collection_interval
   }
