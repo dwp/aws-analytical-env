03dd89ae5c0ad3029f34f4cfaf84c824afe4617f
abdurrahmannuhu <abdurrahman.nuhu@dwp.gsi.gov.ukgit config --global user.name abdurrahmannuhugit config --global user.email abdurrahman.nuhu@dwp.gsi.gov.uk>
2021-01-05 14:11:21 +0000

    DW-5421 fixed formatting

--- terraform/modules/emr/local.tf
+++ terraform/modules/emr/local.tf
@@ -122,7 +122,7 @@ locals {
   cw_agent_log_group_name              = "/app/analytical_batch/get_scripts"
   cw_agent_metrics_collection_interval = 60
   aws_defaut_region                    = "eu-west-2"
-  cw_agent_step_log_group_name        = "/app/analytical_batch/step_logs"
+  cw_agent_step_log_group_name         = "/app/analytical_batch/step_logs"
 
 }
 
