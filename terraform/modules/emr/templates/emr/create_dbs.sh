d8db9a3f911aa72e395022ba68b981bf927c5ae3
PhanindraVenkata <phanindra.venkata@dwp.gsi.gov.uk>
2021-01-20 15:27:20 +0000

    Creating uc_payment_timelines database if not exists

--- terraform/modules/emr/templates/emr/create_dbs.sh
+++ terraform/modules/emr/templates/emr/create_dbs.sh
@@ -7,8 +7,8 @@
 
     PROCESS_ID=$PPID
 
-    PROCESSED_BUCKET="${processed_bucket}"
-    PUBLISHED_BUCKET="${published_bucket}"
+    PROCESSED_BUCKET="${processed_bucket}/data"
+    PUBLISHED_BUCKET="${published_bucket}/data"
 
     START_MESSAGE="Start_creating_databases"
     echo $START_MESSAGE
@@ -17,10 +17,10 @@
     #create UC_Lab databasess
     hive -e "CREATE DATABASE IF NOT EXISTS uc_lab_staging LOCATION '$PROCESSED_BUCKET';"
     hive -e "CREATE DATABASE IF NOT EXISTS uc_lab LOCATION '$PUBLISHED_BUCKET';"
+    hive -e "CREATE DATABASE IF NOT EXISTS uc_payment_timelines LOCATION  '$PUBLISHED_BUCKET';"
 
     END_MESSAGE="Finish_creating_databases"
     echo $END_MESSAGE
     log_message $END_MESSAGE "INFO" "NOT_SET" $PROCESS_ID "batch_emr" "create_dbs.sh" "NOT_SET"
 
 )  >> /var/log/batch/create_dbs.log 2>&1
-
