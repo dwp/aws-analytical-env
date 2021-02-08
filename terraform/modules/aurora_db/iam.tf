cffab59b928449e911b9a6bdaef82e77cef4dd99
IanUtley <ianutley@digital.uc.dwp.gov.uk>
2021-02-08 12:44:12 +0000

    .

--- terraform/modules/aurora_db/iam.tf
+++ terraform/modules/aurora_db/iam.tf
@@ -130,7 +130,7 @@ data "aws_iam_policy_document" "sync_rds" {
     sid       = "AllowRdsDataExecute"
     effect    = "Allow"
     actions   = ["rds-data:ExecuteStatement"]
-    resources = [*]
+    resources = ["*"]
   }
 
   statement {
