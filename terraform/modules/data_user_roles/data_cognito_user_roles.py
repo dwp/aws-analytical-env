995f2a0e13b9901664a65505804c49acff86943f
caldwelltom <76474731+caldwelltom@users.noreply.github.com>
2021-02-11 15:30:13 +0000

    Dw 5598 switch to rbac2 for development (#296)
    * tags to bucket objects
    
    * Dw 5295 mapping users to user iam roles (#211)
    
    * Add user role terraform external data source
    
    * Add user roles to emr security configuration
    
    * adding predictable role names to POC
    
    * python added to image used for tf ci
    
    * adding python and boto3 to ci container and passing in region to script
    
    * rebase
    
    * region added to session
    
    * hard coded region added to session
    
    * using data.aws_region for region input to script
    
    * PR comment
    
    Co-authored-by: Dragos Costache <dragos.costache@infinityworks.com>
    
    * User based configs for release branch (#217)
    
    * adding in missing tags (#212)
    
    * DW-5338 fix permission error for coping files (#214)
    
    Co-authored-by: abdurrahmannuhu <abdurrahman.nuhu@dwp.gsi.gov.ukgit config --global user.name abdurrahmannuhugit config --global user.email abdurrahman.nuhu@dwp.gsi.gov.uk>
    
    * Fix rotate client credentials for jq unsafe client names (#215)
    
    * Dw 5295 mapping users to user iam roles (#211)
    
    * Add user role terraform external data source
    
    * Add user roles to emr security configuration
    
    * adding predictable role names to POC
    
    * python added to image used for tf ci
    
    * adding python and boto3 to ci container and passing in region to script
    
    * rebase
    
    * region added to session
    
    * hard coded region added to session
    
    * using data.aws_region for region input to script
    
    * PR comment
    
    Co-authored-by: Dragos Costache <dragos.costache@infinityworks.com>
    
    * rebase onto master and removing group config
    
    * AuthorizationConfiguration fix
    
    Co-authored-by: abdu-n <57192333+abdu-n@users.noreply.github.com>
    Co-authored-by: abdurrahmannuhu <abdurrahman.nuhu@dwp.gsi.gov.ukgit config --global user.name abdurrahmannuhugit config --global user.email abdurrahman.nuhu@dwp.gsi.gov.uk>
    Co-authored-by: draagc-iw <55280269+draagc-iw@users.noreply.github.com>
    Co-authored-by: Dragos Costache <dragos.costache@infinityworks.com>
    
    * toggle development to use rbac 2
    
    * toggled development back to rbac version 1
    
    * PR review corrections
    
    Co-authored-by: MattToczek <toczek.matt@gmail.com>
    Co-authored-by: MattToczek <52243130+MattToczek@users.noreply.github.com>
    Co-authored-by: Dragos Costache <dragos.costache@infinityworks.com>
    Co-authored-by: abdu-n <57192333+abdu-n@users.noreply.github.com>
    Co-authored-by: abdurrahmannuhu <abdurrahman.nuhu@dwp.gsi.gov.ukgit config --global user.name abdurrahmannuhugit config --global user.email abdurrahman.nuhu@dwp.gsi.gov.uk>
    Co-authored-by: draagc-iw <55280269+draagc-iw@users.noreply.github.com>
    Co-authored-by: thomasstock-caldwell <thomas.stock-caldwell@engineering.digital.dwp.gov.uk>

---
+++ terraform/modules/data_user_roles/data_cognito_user_roles.py
@@ -0,0 +1,68 @@
+import sys
+import boto3
+import json
+import math
+from functools import reduce
+
+
+def get_session(role_arn, region):
+    creds = boto3.client('sts').assume_role(
+        RoleArn=role_arn,
+        RoleSessionName="CognitoUserGetIamRoles"
+    )['Credentials']
+
+    return boto3.Session(
+        aws_access_key_id=creds['AccessKeyId'],
+        aws_secret_access_key=creds['SecretAccessKey'],
+        aws_session_token=creds['SessionToken'],
+        region_name=region
+    )
+
+
+def get_cognito_users(cognitoidp_client, user_pool_id):
+    res = cognitoidp_client.list_users(
+        UserPoolId=user_pool_id,
+        Filter="status=\"Enabled\""
+    )
+    return res['Users']
+
+
+def get_roles_for_users(usernames, account_id):
+    return dict(map(lambda user: (user, f'arn:aws:iam::{account_id}:role/emrfs_{user}'), usernames))
+
+def get_attribute(attributes, name):
+    for attribute in attributes:
+        if attribute.get('Name') == name:
+            return attribute.get('Value')
+    return None
+
+def main():
+    tf_input = json.loads(sys.stdin.read())
+
+    for var in ['role', 'user_pool_id', 'account_id', 'region']:
+        if var not in tf_input:
+            sys.stderr.write("Missing required variable {}".format(var))
+            sys.exit(1)
+
+    session = get_session(tf_input['role'], tf_input['region'])
+
+    users = get_cognito_users(session.client('cognito-idp'), tf_input['user_pool_id'])
+
+    for user in users:
+        username = get_attribute(user.get('Attributes'), 'preferred_username') or user.get('Username')
+        sub = get_attribute(user.get('Attributes'), 'sub')[0:3]
+        user['username_sub'] = f'{username}{sub}'
+
+    users_with_roles = get_roles_for_users(map(lambda user_obj: user_obj['username_sub'], users), tf_input['account_id'])
+
+    result = {
+        'users': json.dumps(users_with_roles),
+        'num_security_configs': str(int(math.ceil(
+            sum(map(lambda key: len(key) + len(users_with_roles[key]) + 100, users_with_roles.keys())) / 60000.0)))
+    }
+
+    print(json.dumps(result))
+
+
+if __name__ == '__main__':
+    main()
