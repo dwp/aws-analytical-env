bd04f4e919a6aeccca3347de4944284a2be2469f
PhanindraVenkata <phanindra.venkata@dwp.gsi.gov.uk>
2021-03-05 11:49:53 +0000

    changes against testing

--- terraform/modules/emr/templates/emr/check_status.sh
+++ terraform/modules/emr/templates/emr/check_status.sh
@@ -2,69 +2,93 @@
 DATA_PRODUCT=$1
 DEPENDANT_DATA_PRODUCT=$2
 DURATION=$3
+INTERVAL=$4
 
 if [ -z "${DURATION}" ]
-  then 
+  then
     END_TIME=80 #Seconds    ##Polls for 5 Hours then exits with Failure
   else
     END_TIME=${DURATION}    ##Polls for variable duration then exits with Failure
 fi
 
-INTERVAL=40 #Seconds     ##Checks for file flag file every 5 Minutes
+if [ -z "${INTERVAL}" ]
+then
+     INTERVAL=40 #Seconds     ##Checks for file flag file every 5 Minutes
+fi
 
 DATE=$(date '+%Y-%m-%d')
 
 while ((${SECONDS} < ${END_TIME}))
 do
   #Get the status of the latest pdm run
-  DEPENDANT_DATA_PRODUCT_STATUS=$(hive -S -e "USE audit; SELECT Status FROM data_pipeline_metadata_hive dp INNER JOIN (
-   SELECT MAX(to_date(dateproductrun)) AS run_date FROM data_pipeline_metadata_hive WHERE dataproduct = '${DEPENDANT_DATA_PRODUCT}') md
-   ON to_date(dp.dateproductrun) = md.run_date
-    WHERE dp.dataproduct = '${DEPENDANT_DATA_PRODUCT}';")
-  if [[ ${DEPENDANT_DATA_PRODUCT_STATUS^^} == "COMPLETED" ]]
+
+  MAX_DATE=$(hive -e "SELECT MAX(to_date(dateproductrun)) AS run_date FROM audit.data_pipeline_metadata_hive WHERE dataproduct = '${DEPENDANT_DATA_PRODUCT}';")
+  DEPENDANT_DATA_PRODUCT_STATUS=$(hive -e "SELECT max(Status) FROM audit.data_pipeline_metadata_hive WHERE dataproduct = '${DEPENDANT_DATA_PRODUCT}' and to_date(dateproductrun) = '${MAX_DATE}';")
+
+  echo $DEPENDANT_DATA_PRODUCT_STATUS
+  echo $MAX_DATE
+
+  if [[ ${DEPENDANT_DATA_PRODUCT_STATUS^^} == *"COMPLETED"* ]]
   then
    break
-  else
-    echo "$DEPENDANT_DATA_PRODUCT is running... Still waiting."
+  elif [[ ${DEPENDANT_DATA_PRODUCT_STATUS^^} == "IN-PROGRESS" ]]
+  then
+       echo "$DEPENDANT_DATA_PRODUCT is running... Still waiting."
+
+  elif [[ ${DEPENDANT_DATA_PRODUCT_STATUS^^} == "FAILED" ]]
+  then
+       echo "$DEPENDANT_DATA_PRODUCT is FAILED. Exiting as a Failure - Please check previous dependencies for errors."
+       exit 1
   fi
+
+  echo "Sleeping for ${INTERVAL} secs"
+
   sleep ${INTERVAL}
 done
 
-if [[ ${DEPENDANT_DATA_PRODUCT_STATUS^^} != "COMPLETED" ]]
-then
- echo "PDM did not finish. Exiting as a Failure - Please check previous dependencies for errors."
- exit 1
-fi
+  echo "Getting the Latest Correlation ID for $DEPENDANT_DATA_PRODUCT"
 
 #Get the correlation_id of the latest PDM run
-  LATEST_COR_ID=$(hive -S -e "USE audit; SELECT correlation_id FROM data_pipeline_metadata_hive dp INNER JOIN (
-   SELECT MAX(to_date(dateproductrun)) AS run_date FROM data_pipeline_metadata_hive WHERE dataproduct = '${DEPENDANT_DATA_PRODUCT}') md
-   ON to_date(dp.dateproductrun) = md.run_date
-    WHERE dp.dataproduct = '${DEPENDANT_DATA_PRODUCT}' AND upper(dp.status) = 'COMPLETED';") 
+  LATEST_COR_ID=$(hive -e "SELECT max(correlation_id) FROM audit.data_pipeline_metadata_hive
+                              WHERE to_date(dateproductrun) = '${MAX_DATE}'
+                              and dataproduct = '${DEPENDANT_DATA_PRODUCT}'
+                              AND upper(status) = 'COMPLETED';")
 
+  echo "LATEST_COR_ID is ${LATEST_COR_ID}"
+
+  echo $LATEST_COR_ID > ~/${DATA_PRODUCT}_CORRELATION_ID.txt
+
+  echo "Fetching the RUN_ID for ${DATA_PRODUCT} and ${LATEST_COR_ID}"
   #RUN_ID will not be set if the data product did not run after PDM has completed
-  RUN_ID=$(hive -S -e "USE audit; SELECT run_id from data_pipeline_metadata_hive WHERE dataproduct = '${DATA_PRODUCT}' AND upper(status) = 'COMPLETED' AND correlation_id = '${LATEST_COR_ID}';")
+  RUN_ID=$(hive -e "USE audit; SELECT run_id from data_pipeline_metadata_hive WHERE dataproduct = '${DATA_PRODUCT}' AND upper(status) = 'COMPLETED' AND correlation_id = '${LATEST_COR_ID}';")
+  echo "RUN_ID is ${RUN_ID} "
 
+
+  echo "Fetching the RUN_STATUS  for ${DATA_PRODUCT} and ${LATEST_COR_ID}"
   #Check for FAILED run
-  RUN_STATUS=$(hive -S -e "USE audit; SELECT status FROM data_pipeline_metadata_hive WHERE dataproduct = '${DATA_PRODUCT}' AND upper(status) = 'FAILED' AND correlation_id = '${LATEST_COR_ID}';")
+  RUN_STATUS=$(hive -e "USE audit; SELECT status FROM data_pipeline_metadata_hive WHERE dataproduct = '${DATA_PRODUCT}' AND upper(status) = 'FAILED' AND correlation_id = '${LATEST_COR_ID}';")
+  echo "RUN_STATUS is ${RUN_STATUS} "
 
   if [  -z ${RUN_ID} ]
   then
     echo "$DEPENDANT_DATA_PRODUCT has finished, Dependencies have finished - Proceeding with flow"
     echo "correlation_id is :"$LATEST_COR_ID
+    echo "Assigning the new Run Id to 1 "
     RUN_ID=1
-    echo "Run Id is: "$RUN_ID
+    echo "Calling update_dynamo.sh"
     ./update_dynamo.sh $LATEST_COR_ID "In-Progress" $DATA_PRODUCT $RUN_ID #Logging
   elif [ ${RUN_STATUS^^} == "FAILED" ]
   then
-    echo "PDM is not running ............... "
-    echo "Re-Running $DATA_PRODUCT after FAILURE"
+    echo "${DEPENDANT_DATA_PRODUCT} is not running ............... "
+    echo "Re-Running ${DATA_PRODUCT} after FAILURE"
     echo "correlation_id is :"$LATEST_COR_ID
+    echo "Incrementing the RUN_ID "
     RUN_ID=$(($RUN_ID+1)) #Increment run id for logging
-    echo "Run Id is: "$RUN_ID
+    echo "New Run Id is: "$RUN_ID
+    echo "Calling update_dynamo.sh"
     ./update_dynamo.sh $LATEST_COR_ID "In-Progress" $DATA_PRODUCT $RUN_ID #Logging
   elif [ ${RUN_STATUS^^} == "IN-PROGRESS" ]
   then
-  echo "$DATA_PRODUCT is running ............... "
+  echo "$DATA_PRODUCT is running .... Exiting the process"
   exit 0
 fi
--- terraform/modules/emr/templates/emr/update_dynamo.sh
+++ terraform/modules/emr/templates/emr/update_dynamo.sh
@@ -5,18 +5,26 @@ STATUS=$2
 DATA_PRODUCT=$3
 RUN_ID=$4
 
+export AWS_DEFAULT_REGION=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | grep region | cut -d'"' -f4)
+
+
 if [  -z ${RUN_ID} ]
 then
     response=`aws dynamodb get-item --table-name data_pipeline_metadata --key '{"Correlation_Id": {"S": "'$CORRELATION_ID'"}, "DataProduct": {"S": "'$DATA_PRODUCT'"}}'`
     RUN_ID=`echo $response | jq -r .'Item.Run_Id.N'`
 fi
 
+if [  -z ${CORRELATION_ID} ]
+then
+    CORRELATION_ID=`cat ~/${DATA_PRODUCT}_CORRELATION_ID.txt`
+fi
+
 CLUSTER_ID=`cat /mnt/var/lib/info/job-flow.json | jq '.jobFlowId'`
-CLUSTER_ID=$${CLUSTER_ID//\"}
+CLUSTER_ID=${CLUSTER_ID//\"}
 
 DATE=$(date '+%Y-%m-%d')
 
-aws dynamodb update-item \
+$(which aws) dynamodb update-item \
     --table-name data_pipeline_metadata \
     --key "{\"Correlation_Id\":{\"S\":\"$CORRELATION_ID\"},\"DataProduct\":{\"S\":\"$DATA_PRODUCT\"}}" \
     --update-expression "SET Correlation_id = :c, #a = :x, #d = :w, Run_Id = :z, Cluster_Id = :u"  \
