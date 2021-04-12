ded39dca944b0e4df19a4b55cbf7f8cdf40b2cc8
PhanindraVenkata <phanindra.venkata@dwp.gsi.gov.uk>
2021-04-12 15:58:41 +0100

    correcting the variables for template file

--- terraform/modules/emr-launcher/s3.tf
+++ terraform/modules/emr-launcher/s3.tf
@@ -76,20 +76,16 @@ resource "aws_s3_bucket_object" "batch_instances" {
   bucket = var.config_bucket.id
   key    = "emr/batch-cluster/instances.yaml"
   content = templatefile("../../../batch_cluster_config/instances.yaml.tpl", {
-    common_security_group       = var.common_security_group
-    master_security_group       = var.master_security_group
-    slave_security_group        = var.slave_security_group
-    service_security_group      = var.service_security_group
-    subnet_ids                  = join(",", var.subnet_ids)
-    core_instance_count         = var.core_instance_count
-    instance_type_master        = var.instance_type_master
-    instance_type_core_one      = var.instance_type_core_one
-    instance_type_core_two      = var.instance_type_core_two
-    instance_type_core_three    = var.instance_type_core_three
-    hive_compaction_threads     = var.hive_compaction_threads
-    hive_tez_sessions_per_queue = var.hive_tez_sessions_per_queue
-    hive_max_reducers           = var.hive_max_reducers
-
+    common_security_group    = var.common_security_group
+    master_security_group    = var.master_security_group
+    slave_security_group     = var.slave_security_group
+    service_security_group   = var.service_security_group
+    subnet_ids               = join(",", var.subnet_ids)
+    core_instance_count      = var.core_instance_count
+    instance_type_master     = var.instance_type_master
+    instance_type_core_one   = var.instance_type_core_one
+    instance_type_core_two   = var.instance_type_core_two
+    instance_type_core_three = var.instance_type_core_three
   })
   tags = merge(var.common_tags, { Name : "${var.name_prefix}-emr-launch-instances" })
 }
@@ -116,6 +112,9 @@ resource "aws_s3_bucket_object" "batch_configurations" {
     hive_metastore_username      = var.hive_metastore_username
     hive_metastore_secret_id     = var.hive_metastore_secret_id
     environment                  = var.environment
+    hive_compaction_threads      = var.hive_compaction_threads
+    hive_tez_sessions_per_queue  = var.hive_tez_sessions_per_queue
+    hive_max_reducers            = var.hive_max_reducers
   })
   tags = merge(var.common_tags, { Name : "batch-emr-launch-config" })
 }
