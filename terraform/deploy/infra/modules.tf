b52f89085747d2c5886023c049e1783d96d20edc
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2021-05-06 12:03:00 +0100

    Add metrics SG as a VPCE SG

--- terraform/deploy/infra/modules.tf
+++ terraform/deploy/infra/modules.tf
@@ -4,7 +4,9 @@ module analytical_env_vpc {
 
   common_tags                              = local.common_tags
   gateway_vpce_route_table_ids             = module.networking.outputs.aws_route_table_private_ids
-  interface_vpce_source_security_group_ids = []
+  interface_vpce_source_security_group_ids = [
+      data.terraform_remote_state.dataworks_metrics_infrastructure.outputs.outputs.clive_pushgateway_security_group
+  ]
   interface_vpce_subnet_ids                = module.networking.outputs.aws_subnets_private[*].id
   region                                   = data.aws_region.current.id
   vpc_cidr_block                           = local.cidr_block[local.environment]["aws-analytical-env-vpc"]
