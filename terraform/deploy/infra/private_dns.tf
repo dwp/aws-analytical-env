069a77d974e52a852f4c4453e7ba3e5daecdb663
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2021-06-02 16:00:42 +0100

    TF FMT

--- terraform/deploy/infra/private_dns.tf
+++ terraform/deploy/infra/private_dns.tf
@@ -1,11 +1,16 @@
+data "aws_secretsmanager_secret_version" "terraform_secrets" {
+  provider  = aws.management_dns
+  secret_id = "/concourse/dataworks/terraform"
+}
+
 resource "aws_service_discovery_private_dns_namespace" "azkaban_services" {
-  name  = "${local.environment}.azkaban.services.${var.parent_domain_name}"
-  vpc   = module.analytical_env_vpc.vpc.id
-  tags  = merge(local.common_tags, { Name = "azkaban_services" })
+  name = "${local.environment}.azkaban.services.${jsondecode(data.aws_secretsmanager_secret_version.terraform_secrets.secret_binary)["dataworks_domain_name"]}"
+  vpc  = module.analytical_env_vpc.vpc.id
+  tags = merge(local.common_tags, { Name = "azkaban_services" })
 }
 
 resource "aws_service_discovery_service" "azkaban_services" {
-  name  = "azkaban_services"
+  name = "azkaban_services"
 
   dns_config {
     namespace_id = aws_service_discovery_private_dns_namespace.azkaban_services[0].id
