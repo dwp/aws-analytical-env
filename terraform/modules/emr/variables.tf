a2c7c462ad51a013f4fdaeffb7cb466981abffba
PhanindraVenkata <phanindra.venkata@dwp.gsi.gov.uk>
2021-06-28 11:56:12 +0100

    removing variable

--- terraform/modules/emr/variables.tf
+++ terraform/modules/emr/variables.tf
@@ -303,8 +303,3 @@ variable "hive_custom_auth_provider_path" {
   type        = string
   description = "Local file path of the hive custom auth provider jar"
 }
-
-variable "hive_tez_container_size" {
-  type        = string
-  description = "This property specifies tez container size "
-}
