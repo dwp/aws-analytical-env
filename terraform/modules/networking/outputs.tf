ee4327ff417aa86840a23e700ad9d06b05cbcfff
steveburton4 <Steven.burton@hotmail.co.uk>
2021-07-02 10:12:30 +0100

    Revert "enable subnet for emr template compatibility (#442)" (#443)
    This reverts commit 36e5caeef55f567f0c51fa82f0b323cab5cb33e9.

--- terraform/modules/networking/outputs.tf
+++ terraform/modules/networking/outputs.tf
@@ -6,8 +6,7 @@ output outputs {
     aws_subnets_public = {
       ids = aws_subnet.public.*.id
     }
-    aws_vpc                                    = var.vpc
-    aws_route_table_private_ids                = aws_route_table.private[*].id
-    aws_vpc_peering_connection_accepter_crypto = aws_vpc_peering_connection_accepter.crypto
+    aws_vpc                     = var.vpc
+    aws_route_table_private_ids = aws_route_table.private[*].id
   }
 }
