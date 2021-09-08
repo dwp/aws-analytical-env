315c0434cf9bef1335949fc4ec1851393973ac4f
MattToczek <matthew.toczek@dmwgroup.co.uk>
2021-09-08 16:40:10 +0100

    dev test of cw alarm

--- terraform/deploy/app/locals.tf
+++ terraform/deploy/app/locals.tf
@@ -2,7 +2,7 @@ locals {
   azkaban_pushgateway_hostname = "${data.terraform_remote_state.aws_analytical_environment_infra.outputs.private_dns.azkaban_service_discovery.name}.${data.terraform_remote_state.aws_analytical_environment_infra.outputs.private_dns.azkaban_service_discovery_dns.name}"
 
   emr_launcher_failure_alert = {
-    development = 0
+    development = 1
     qa          = 0
     integration = 0
     preprod     = 1
