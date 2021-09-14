3a027a7b64e35aefac6c2bb8e3bd6908083e39df
Daniel Chicot <dan.chicot@gmail.com>
2021-09-14 11:26:56 +0100

    Shared files to create egress control file in azkaban jobs. (#542)
    * Shared files to create egress control file in azkaban jobs.
    
    * Terraform fmt.
    
    Co-authored-by: steveburton4 <Steven.burton@hotmail.co.uk>

--- terraform/modules/emr/files/azkaban/metadata/run.sh
+++ terraform/modules/emr/files/azkaban/metadata/run.sh
@@ -2,4 +2,5 @@
 
 source "${AZKABAN_JOB_UTILITY_HOME:-/opt/emr/azkaban}/metadata/environment.sh"
 
-metadata::metadata "${1:?Usage $(basename "$0") input-directory}"
+metadata::metadata "${1:?Usage $(basename "$0") input-directory output-file}" \
+  > "${2:?Usage $(basename "$0") input-directory output-file}"
