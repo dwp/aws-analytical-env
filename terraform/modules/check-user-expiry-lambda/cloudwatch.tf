5ed970cc878128fb0419eb3de7c04d6055cc960b
ekgyimah <gyimah-emmy@hotmail.it>
2021-12-01 15:36:44 +0000

    Cloudwatch group updated

--- terraform/modules/check-user-expiry-lambda/cloudwatch.tf
+++ terraform/modules/check-user-expiry-lambda/cloudwatch.tf
@@ -1,4 +1,10 @@
 resource "aws_cloudwatch_log_group" "check_user_expiry_lambda_logs" {
+  name              = "${var.name_prefix}-check-user-expiry-lambda-logs"
+  retention_in_days = 180
+  tags              = merge(var.common_tags, { Name : "${var.name_prefix}-expiry-logs" })
+}
+
+resource "aws_cloudwatch_log_group" "check_user_expiry_lambda_logs_updated" {
   name              = "/aws/lambda/${var.name_prefix}-check-user-expiry"
   retention_in_days = 180
   tags              = merge(var.common_tags, { Name : "${var.name_prefix}-expiry-logs" })
