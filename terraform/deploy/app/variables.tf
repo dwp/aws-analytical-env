5672b2a14af2f1e4bb0af0753cefd10e7868ba3e
rbryett <13303701+rbryett@users.noreply.github.com>
2023-04-05 09:43:20 +0100

    Dtwrks 986 (#673)
    * DTWRKS-986 propagate HCS tagging to EMR cluster
    
    * DTWRKS-986 propagate HCS tagging to EMR cluster
    
    * DTWRKS-986 propagate HCS tagging to EMR cluster
    
    * DTWRKS-986 propagate HCS tagging to EMR cluster
    
    * DTWRKS-986 propagate HCS tagging to EMR cluster
    
    ---------
    
    Co-authored-by: rorybryett <rory.bryett@dwp.gov.uk>

--- terraform/deploy/app/variables.tf
+++ terraform/deploy/app/variables.tf
@@ -8,11 +8,6 @@ variable "region" {
   default = "eu-west-2"
 }
 
-variable "costcode" {
-  type    = string
-  default = "PRJ0022507"
-}
-
 variable "aws_analytical_env_emr_launcher_zip" {
   type = map(string)
 
