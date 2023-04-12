badb753ec7714a7173eb73e90d367898d02805a3
Jonathan Melia <jonathan.melia@engineering.digital.dwp.gov.uk>
2023-04-12 15:24:54 +0100

    chore: Adding bootstrap args

--- terraform/modules/emr/data.tf
+++ terraform/modules/emr/data.tf
@@ -12,3 +12,16 @@ data "archive_file" "emr_scheduled_scaling_lambda_zip" {
   source_dir  = "${path.module}/emr-scheduled-scaling-lambda-files"
   output_path = "${path.module}/${var.name_prefix}-emr-scheduled-scaling.zip"
 }
+
+data "terraform_remote_state" "internal_compute" {
+  backend   = "s3"
+  workspace = terraform.workspace
+
+  config = {
+    bucket         = "{{terraform.state_file_bucket}}"
+    key            = "terraform/dataworks/aws-internal-compute.tfstate"
+    region         = "{{terraform.state_file_region}}"
+    encrypt        = true
+    kms_key_id     = "arn:aws:kms:{{terraform.state_file_region}}:{{terraform.state_file_account}}:key/{{terraform.state_file_kms_key}}"
+    dynamodb_table = "remote_state_locks"
+  }
