a38cdb8bc36368b6f5869259da72ce1a59ecf447
Jonathan Melia <jonathan.melia@engineering.digital.dwp.gov.uk>
2023-04-12 15:27:37 +0100

    chore: Adding bootstrap args

--- terraform/modules/emr/data.tf
+++ terraform/modules/emr/data.tf
@@ -25,3 +25,4 @@ data "terraform_remote_state" "internal_compute" {
     kms_key_id     = "arn:aws:kms:{{terraform.state_file_region}}:{{terraform.state_file_account}}:key/{{terraform.state_file_kms_key}}"
     dynamodb_table = "remote_state_locks"
   }
+}
