5e16c17aa2bf4395482c82965908968106ac3a7f
Jonathan Melia <jonathan.melia@engineering.digital.dwp.gov.uk>
2023-05-17 12:54:31 +0100

    fix: batch compute create before destroy

--- terraform/modules/testing/create_metrics_data_batch_job.tf
+++ terraform/modules/testing/create_metrics_data_batch_job.tf
@@ -26,6 +26,10 @@ resource "aws_batch_compute_environment" "create_metrics_data_environment" {
       version            = aws_launch_template.create_metrics_data_environment.latest_version
     }
 
+    lifecycle {
+      create_before_destroy = true
+    }
+
     tags = merge(
       var.common_tags,
       {
