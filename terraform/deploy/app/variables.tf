d917dd7e7be375a8f1a6fbb4828115de52d15705
Sumit Bharadia <36968381+03ce007@users.noreply.github.com>
2023-05-18 17:31:57 +0100

    upgrade emr version for lower env
    Signed-off-by: Sumit Bharadia <36968381+03ce007@users.noreply.github.com>

--- terraform/deploy/app/variables.tf
+++ terraform/deploy/app/variables.tf
@@ -12,9 +12,9 @@ variable "emr_release" {
   default = {
     development = "6.3.0"
     qa          = "6.3.0"
-    integration = "6.2.0"
-    preprod     = "6.2.0"
-    production  = "6.2.0"
+    integration = "6.3.0"
+    preprod     = "6.3.0"
+    production  = "6.3.0"
   }
 }
 variable "aws_analytical_env_emr_launcher_zip" {
