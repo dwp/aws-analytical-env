5531967ecd474274d09835ac29fd6292b3bd999c
Jonathan Melia <jonathan.melia@engineering.digital.dwp.gov.uk>
2023-06-13 09:29:06 +0100

    refactor: Trend/tanium config

--- terraform/deploy/app/modules.tf
+++ terraform/deploy/app/modules.tf
@@ -210,6 +210,17 @@ module "launcher" {
   hive_max_reducers                     = var.emr_hive_max_reducers[local.environment]
   alarm_sns_arn                         = data.terraform_remote_state.security-tools.outputs.sns_topic_london_monitoring.arn
   alarm_on_failure                      = local.emr_launcher_failure_alert[local.environment]
+  install_tenable                       = local.tenable_install[local.environment]
+  install_trend                         = local.trend_install[local.environment]
+  install_tanium                        = local.tanium_install[local.environment]
+  tanium_server_1                       = local.tanium1
+  tanium_server_2                       = local.tanium2
+  tanium_env                            = local.tanium_env[local.environment]
+  tanium_log_level                      = local.tanium_log_level[local.environment]
+  tenant                                = local.tenant
+  tenantid                              = local.tenantid
+  token                                 = local.token
+  policyid                              = local.policy_id[local.environment]
 }
 
 module "emrfs_lambda" {
