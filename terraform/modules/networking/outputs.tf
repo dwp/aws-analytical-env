70bbffd91a3b222c01164c0e45b6b467a31bd8d1
sumit.bharadia <sumit.bharadia@engineering.digital.dwp.gov.uk>
2023-07-25 08:56:09 +0100

    chore: tanium vpc endpoint

--- terraform/modules/networking/outputs.tf
+++ terraform/modules/networking/outputs.tf
@@ -10,3 +10,11 @@ output "outputs" {
     aws_route_table_private_ids = aws_route_table.private[*].id
   }
 }
+
+output "tanium_service_endpoint" {
+  value = {
+    id  = aws_vpc_endpoint.tanium_service.id
+    dns = aws_vpc_endpoint.tanium_service.dns_entry[0].dns_name
+    sg  = aws_security_group.tanium_service_endpoint.id
+  }
+}
